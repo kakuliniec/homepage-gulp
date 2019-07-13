6b0cffd3df8824e4ca208e686a7fb1f2120ea1f0
Kaka K <kakuliniec@wp.pl>
2019-07-13 16:17:36 +0200

    feat:moving to gulp

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,95 @@
 "use strict";
 
-console.log(`Hello world!`);
+console.log('Czołem! :)');
+const name = 'Kaka';
+const age = 33;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lata`);
+console.log('Siemano');
+
+const about = document.querySelector('.about__paragraph--js');
+console.log(about.innerHTML);
+about.innerHTML = 'Pozdro <strong>JS</strong>';
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+console.log(paragraphs[0]);
+if ('JavaScript'!= 'Java') {
+    console.log('to prawda!')
+}
+if (age < 20) {
+console.log('masz mniej niz 20 lat');
+} else if ((age >= 20) && (age <= 30)) {
+    console.log('masz wiecej niz 20 lat, ale mniej niz 30 lat')
+
+} else {
+console.log('jestes 31+');
+}
+switch (age) {
+    case 20:
+console.log('masz równo 20 lat');
+    break;
+    case 30:
+        console.log('masz równo 30 lat');
+        break;
+        default:
+                console.log(`masz ${age} lata`);
+                break;
+}
+const amIOld = (age > 70) ? 'yes' : 'n';
+console.log(amIOld);
+
+function calculate(x) {
+    x = x + 3;
+    console.log(`tracycyjnie ${x}`);
+    return x*7
+}
+
+console.log(calculate(2));
+
+
+const calculatefat = x => (x+3)*7;
+
+console.log(calculatefat(2));
+
+function writeName(yourname, yourage) {
+    console.log(yourname, yourage);
+}
+writeName("Kaka", 33);
+
+const welcome = (name, age) => {
+    console.log(`Witaj ${name},masz ${age} lata`);
+
+}
+welcome('Koko', 33);
+
+const button = document.querySelector('.main__button--js');
+console.log(button);
+
+function handleClick() {
+    console.log('halo');
+}
+
+button.addEventListener('click', (e) => {
+    const header = document.querySelector('.main__header--js');
+    header.innerHTML = 'klik klik';
+    header.classList.toggle('main__header--pink');
+    if  (header.classList.contains('main__header--pink')) {
+        console.log('jest klasa');
+    }
+    else {
+    console.log('brak klasy');}
+})
+
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visable');
+})
+
+
---
+++ src/js/objects.js
@@ -0,0 +1,16 @@
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+     console.log(`${target} destroyed 💥`)
+     },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000,
+    crews: {
+        role: 'captain',
+        name: 'deathStar captain'
+    }
+    }
+
+    console.log(deathStar)
